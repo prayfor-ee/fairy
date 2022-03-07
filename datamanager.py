e0fd3b1958df26bd6b6b89664c218819497f8f4e
prayfor.ee <prayfor.ee@gmail.com>
2022-03-07 23:41:36 +0900

    [datamanager] - impl. call api

--- datamanager.py
+++ datamanager.py
@@ -3,7 +3,7 @@ import common as C
 import apikey as API_KEY
 import requesturl as REQUEST_URL
 
-import datatime
+import datetime
 import requests
 import json
 
@@ -17,7 +17,41 @@ class DataManager:
 
 	def request_weather(self):
 		#단기예보
-		request_server = REQUEST_URL.DATA_GO_VILAGE_FCST
+		request_url = REQUEST_URL.DATA_GO_VILAGE_FCST
 
 		#초단기예보
-		#request_server = REQUEST_URL.DATA_GO_ULTRA_SRT_FCST
+		#request_url = REQUEST_URL.DATA_GO_ULTRA_SRT_FCST
+
+		today = datetime.datetime.today()
+		base_date = today.strftime("%Y%m%d") # "20220307" == 기준 날짜
+		base_time = "0500" # 날씨 값
+
+		nx = "62"
+		ny = "123"
+		
+		params = {'serviceKey' : API_KEY.DATA_GO_DECODING_KEY, 
+		#'pageNo' : '50', 
+		#'numOfRows' : '12', #Total 12 / 
+		'dataType' : 'json', 
+		'base_date' : base_date, 
+		'base_time' : base_time, 
+		'nx' : nx, 
+		'ny' : ny }
+
+		res = requests.get(request_url, params=params)
+		
+		if res.status_code == 200 :#normal
+			C.P(f"res.status_code [{res.status_code}]")
+			#C.P(f"res.content [{res.content}]")
+			items = res.json().get('response').get('body').get('items')
+			C.P(f"items [{items}]")
+		else:
+			C.P("error! because ")#, res.json())
+
+		#C.P(f"res.content [{res.content}]")
+
+#params ={'serviceKey' : '서비스키', 'pageNo' : '1', 'numOfRows' : '1000', 'dataType' : 'XML', 'base_date' : '20210628', 'base_time' : '0500', 'nx' : '55', 'ny' : '127' }
+
+#response = requests.get(url, params=params)
+#print(response.content)
+		#items = res.json().get('response').get('body').get('items')
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import common as C
 import sttmanager as STT
-
+import datamanager as DATA
 if __name__ == '__main__':
     C.P('start')
 
     stt_manager = STT.STTManager()
-    audio = stt_manager.get_voice_recognition()
-    text = stt_manager.request_kakao(audio)
-    C.P(f"text [{text}]")
+    #audio = stt_manager.get_voice_recognition()
+    #text = stt_manager.request_kakao(audio)
+    #C.P(f"text [{text}]")
+
+    data_manager = DATA.DataManager()
+    data_manager.request_weather()
 
     C.P('end')
